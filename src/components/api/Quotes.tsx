1d321613e606b9913daee472198263ea43f89aef
Your Name <you@example.com>
2023-02-14 12:14:09 +0200

    quotes component

---
+++ src/components/api/Quotes.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import "./Quotes.css";
+const Quotes = () => {
+  const [quotes, setQuotes] = useState({ text: "", author: "" });
+  const getQuotes = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randNum]);
+      });
+  };
+  useEffect(() => {
+    getQuotes();
+  }, []);
+  return (
+    <div className="quoteContainer m-1">
+      <div className="quote">
+        <p>"{quotes.text}"</p>
+        <p>{quotes.author}</p>
+        <div className="btnContainer">
+            <button onClick={getQuotes} className="btnQuote">Get Quote</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Quotes;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,4 @@
+import Quotes from "../components/api/Quotes";
 import MultiCarousel from "../components/carousel/MultiCarousel";
 import HeroSection from "../components/hero-section/HeroSection";
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div>
    <HeroSection/>
+   <Quotes/>
    <MultiCarousel/>
     </div>
   );
